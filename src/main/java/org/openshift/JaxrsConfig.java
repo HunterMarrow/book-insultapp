9f4c34dbbe6f5a720919f8232331ae6640b09917
Grant Marrow <grantmarrow@gmail.com>
2019-01-02 19:28:06 +0200

    Added db dependency

---
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -0,0 +1,12 @@
+package org.openshift;
+
+import javax.ws.rs.ApplicationPath;
+import java.util.HashMap;
+import javax.enterprise.context.RequestScoped; import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@ApplicationPath("/api")
+public class JaxrsConfig {
+
+}
